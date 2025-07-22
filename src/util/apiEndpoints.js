a3eaad031f1e7ccc4dffbf637ff7097fddaf0dd9
Roshan_Prakhar <rtgrosh@gmail.com>
2025-07-22 15:09:39 +0530

    fix to deployment

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,5 +1,5 @@
 //export const BASE_URL = "http://localhost:8080/api/v1.0";
-export const BASE_URL = "https://money-manger-7wto.onrender.com/api/v1.0";
+export const BASE_URL = "http://moneymanager-env.eba-5wjjyxhb.ap-south-1.elasticbeanstalk.com/";
 const CLOUDINARY_CLOUD_NAME = "dhadf5h7j";
 
 export const API_ENDPOINTS = {

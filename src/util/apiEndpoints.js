3f5ca6937ec0cc73b93ae65a131f7a3027907f6e
Roshan_Prakhar <rtgrosh@gmail.com>
2025-07-29 10:46:07 +0530

    new

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8080/api/v1.0";
+export const BASE_URL = "http://localhost:8080/";
 //export const BASE_URL = "http://moneymanager-env.eba-5wjjyxhb.ap-south-1.elasticbeanstalk.com/api/v1.0";
 const CLOUDINARY_CLOUD_NAME = "dhadf5h7j";
 
